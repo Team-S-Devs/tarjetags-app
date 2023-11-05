a3b1c5bbf4a373acb2a6ab7b44c8e560a496ae54
santiCaballero <santiago.caballero.6760385@usalesiana.edu.bo>
2023-11-05 03:30:30 -0400

    header

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import image from '../assets/images/logo.png'
 import { Link, useLocation } from 'react-router-dom'
+import { BiSolidUser } from "react-icons/bi";
+import { LiaIdCardSolid } from "react-icons/lia";
+import LinkComponent from '../components/buttons/LinkComponent'
 
 const Header = () => {
   const location = useLocation();
@@ -23,16 +26,17 @@ const Header = () => {
         <Link to='/#home'>
           <div className='logoHeader'>
             <img src={image} alt="vetsoft logo"/>
-            <p><span className='primary-purple'>Vet</span><span className='primary-319c'>Soft</span></p>
           </div>
         </Link>
       </div>
       <nav className={'navigation ' +(conditional)}>
-        <Link to="/clinica"><span className='link'>CLÍNICA</span></Link>
-        <Link to="/peluqueria"><span className='link'>PELUQUERÍA</span></Link>
-        <Link to="/petshop" className='link'><span className='link'>PETSHOP</span></Link>
-        <Link to="/precios" className='link'><span className='link'>PRECIOS</span></Link>
-        <Link to="https://wa.me/message/WP4EUCZXJR3XL1" target='_blank'><span className='link mainButton'>SOLICITA TU DEMO</span></Link>
+
+      <LiaIdCardSolid className='icon-header'/>
+        <Link to="/"><span className='link'>Ver Tarjetas Creadas</span></Link>
+
+        <BiSolidUser className='icon-header'/>
+        <Link to="/"><span className='link'>Ver Perfil</span></Link>
+        <Link to="" target='_blank'><span className='link mainButton'>Chapo Guzman</span></Link>
       </nav>
       <div className='menuButton' onClick={onClickHeader}>
         <button className="headerButton">
